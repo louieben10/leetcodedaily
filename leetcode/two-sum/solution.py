6a59edd72e5894dce153c0faaa70179f512b8a44
louieben10 <121926976+louieben10@users.noreply.github.com>
2026-09-26 18:43:24 +0530

    solution for two-sum

---
+++ leetcode/two-sum/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        h = {}
+        for i in range (len(nums)):
+            h[nums[i]] = i 
+        
+        for i in range (len(nums)):
+            y = target - nums[i]
+            if y in h and h[y] !=i:
+                return [i , h[y]]    
